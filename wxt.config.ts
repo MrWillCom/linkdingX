47793bcb201ca42432f5a82cc9e0dd06473a9a32
Mr. Will <mr.will.com@outlook.com>
2026-03-23 18:07:32 +0800

    Add icon

--- wxt.config.ts
+++ wxt.config.ts
@@ -3,10 +3,14 @@ import tailwindcss from '@tailwindcss/vite'
 
 // See https://wxt.dev/api/config.html
 export default defineConfig({
-  modules: ['@wxt-dev/module-react'],
+  modules: ['@wxt-dev/module-react', '@wxt-dev/auto-icons'],
   vite: () => ({ plugins: [tailwindcss()] }),
   manifest: {
+    name: 'linkdingX',
     permissions: ['storage', 'tabs'],
     host_permissions: ['https://*/*'],
+  },
+  autoIcons: {
+    baseIconPath: 'assets/icon.svg',
   },
 })
